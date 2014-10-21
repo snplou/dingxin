e24a5bfe9137d04a98af263bd5e7b8d5ed113515
snplou <snplou@163.com>
2014-10-20 23:11:00 -0500

    update method added

---
+++ application/views/category/modifyview.php
@@ -0,0 +1,15 @@
+
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+
+<form method="post" action="<?php echo $host_url;?>/index.php/category/modify">
+id: <input type="text"/ name="id" >
+name: <input type="text"/ name="name" >
+<input type="submit" value="提交" >
+</form>
+
+</body>
+</html>
