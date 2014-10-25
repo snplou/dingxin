1b3ef64d0356f7e07ec2572f413802a2a0953301
snplou <snplou@163.com>
2014-10-25 12:14:50 -0500

    add combotree for select a category

--- application/views/article/addView.php
+++ application/views/article/addView.php
@@ -1,12 +1,23 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html;" charset="UTF-8">
+
+<link rel="stylesheet" type="text/css" href="http://localhost/dingxindianqi/jeasyui/themes/default/easyui.css">
+<link rel="stylesheet" type="text/css" href="http://localhost/dingxindianqi/jeasyui/themes/icon.css">
+<script type="text/javascript" src="http://localhost/dingxindianqi/jeasyui/jquery.min.js"></script>
+<script type="text/javascript" src="http://localhost/dingxindianqi/jeasyui/jquery.easyui.min.js"></script>
+
+
 </head>
 
 <body>
 <form action="<?php echo $host_url?>/index.php/article/add" method="post">
 
-    文章类别<input id="articlecat" name="articlecat" type="text">
+    文章类别<input class="easyui-combotree" 
+                    id="articlecategory" 
+                    name="articlecat" 
+                    url="http://localhost/dingxindianqi/index.php/category/treenode" >
+
     文章标题<input id="articletitle" name="articletitle" type="text">
 
     <script id="ueditorcontainer" name="articlecontent" type="text/plain" ></script>
