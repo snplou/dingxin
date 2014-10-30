670a6ff05d5ad18af75ed90ce7e46bc70e9a32e0
snplou <snplou@163.com>
2014-10-30 13:12:54 -0500

    layout for backend_index.html

---
+++ application/controllers/backend_index.php
@@ -0,0 +1,40 @@
+<?php
+
+
+class Backend_index extends CI_Controller{
+
+
+
+    var $host_url;
+
+    function __construct(){
+        parent::__construct();
+        $this->host_url="http://localhost/dingxindianqi";
+        //todo:验证用户登录状态
+        //todo 验证用户权限
+    }
+
+
+    function index(){
+
+        $array=array(
+            "host_url"=>$this->host_url,
+        );
+
+        $this->load->view(
+            "backend_index",
+            $array
+        );
+
+    }
+
+
+
+
+
+}
+
+
+
+?>
+
